f304819131f69525dd6d9c3ab349af68cf14cb11
datle-dev <dle.dev@outlook.com>
2024-03-11 12:28:07 -0400

    Initial commit of webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'source-map',
+    devServer: {
+        static: './dist',
+        watchFiles: [
+            './src/**/*.html',
+            './src/**/*.css',
+            './src/**/*.js',
+        ],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Production',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
